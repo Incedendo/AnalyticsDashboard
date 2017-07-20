568cc257c2ef5c70be43e294a70e7af62bf1c6b5
cconcep <cconcep@purdue.edu>
2017-07-20 14:56:05 -0500

    simple app.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+		projects: []
+	};
+
+  componentDidMount() {
+   const dataAPI = 'http://localhost:3000';
+   axios.get(dataAPI + '/test')
+     .then((response) => {
+       this.setState({
+         projects: response.data
+       });
+     })
+     .catch( (error) => {
+       console.log(error);
+     }
+   );
+  }
+
   render() {
+		const { projects } = this.state;
+    var arr =[]; //Array of arrays
+
+    Object.keys(projects)
+      .map((key) => {
+        const list = projects[key];
+        arr.push(list);
+    });
+
     return (
-      <div className="App">
-        
+      <div>
+
       </div>
     );
   }
