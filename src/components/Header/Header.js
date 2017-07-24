92601b08958e70b306a403895f4ef6aa5ca5263b
Incedendo <nguyenminhkiet94@gmail.com>
2017-07-24 16:01:31 -0500

    header bar almost working

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,32 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 import BurgerMenuComponent from './BurgerMenu/BurgerMenuComponent';
-
-import { slide as Menu } from 'react-burger-menu'
-
-
 import './Header.css'
+import VALIC from '../../assets/svg/VALICWhiteLogo.svg';
 
 const Header = () => {
     return(
-      <div className="header">
-        <div className="inLine">VALIC</div>
-
-        <div className="analytics inLine">ANALYTICS</div>
-
-        <div className="share inLine">SHARE</div>
-
-        <div className="burgerMenu">
-          <Menu right >
-            <a id="home" className="menu-item" href="/">Home</a>
-            <a id="about" className="menu-item" href="/about">About</a>
-            <a id="contact" className="menu-item" href="/contact">Contact</a>
-            <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
-          </Menu>
+        <div className="header">
+          <div className="inLine VALICLogo">
+            <img src={VALIC} />
+          </div>
+          <div className="analytics inLine">ANALYTICS</div>
+          <div className="share inLine">SHARE</div>
         </div>
-
-
-      </div>
     );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
 import {CardMenu} from './CardDisplay/CardMenu';
 import ChartDisplay from './Graphs/ChartDisplay';
-import Header from './Header/Header'
-
+import Header from './Header/Header';
 import PropTypes from 'prop-types';
 import './home.css';
 
@@ -42,19 +40,19 @@ class Home extends Component {
 
     return (
 
-      <div style={{backgroundColor: '#022753'}}>
-        <Header />
+      <div className="mainDiv">
 
-        <div className="mainDiv">
-          <div className="pageVisit">
-            Page Visits
+          <Header />
+          <div className="mainDiv">
+            <div className="pageVisit">
+              Page Visits
+            </div>
+            {this.state.mounted && <ChartDisplay listHome={arr} graphType='line' dataType={dataType} frequency="annually" chartHeight="500px" width="100%" />}
           </div>
-          {this.state.mounted && <ChartDisplay listHome={arr} graphType='line' dataType={dataType} frequency="annually" chartHeight="500px" width="2670px" />}
-        </div>
 
-        <div>
-          <CardMenu list={arr} />
-        </div>
+          <div>
+            <CardMenu list={arr} />
+          </div>
 
       </div>
     );
